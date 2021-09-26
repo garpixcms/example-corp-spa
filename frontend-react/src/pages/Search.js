913bd3848ceba768426a8468a10b9c05ca5a040b
crusat <crusat@yandex.ru>
2021-09-26 22:59:42 +0300

    Поиск

--- frontend-react/src/pages/Search.js
+++ frontend-react/src/pages/Search.js
@@ -1,25 +1,50 @@
+import React, {useState} from 'react';
 import Layout from "../views/Layout";
+import api from '../api';
 
 const SearchPage = (props) => {
-  const { object } = props;
-  return (
-    <Layout {...props}>
-      <h1>{object.title}</h1>
+    const {object, paginated_object_list} = props;
+    const [searchValue, setSearchValue] = useState('');
+    const [paginatedObjectList, setPaginatedObjectList] = useState(paginated_object_list);
 
-      <form method="get">
-        <div className="form-row align-items-center">
-          <div className="col-auto">
-            <label className="sr-only" htmlFor="q">Поиск</label>
-            <input type="text" className="form-control mb-2" name="q" id="q"
-                   placeholder="Введите поисковый запрос..." />
-          </div>
-          <div className="col-auto">
-            <button type="submit" className="btn btn-primary mb-2">Найти</button>
-          </div>
-        </div>
-      </form>
-    </Layout>
-  )
+    const onSubmit = (event) => {
+        console.log(searchValue, object)
+        api.getPage([`/${object.slug}`], {
+            q: searchValue,
+        }).then((res) => {
+            setPaginatedObjectList(res.page.paginated_object_list);
+        })
+        event.preventDefault();
+    }
+
+    return (
+        <Layout {...props}>
+            <h1>{object.title}</h1>
+
+            <form method="get" onSubmit={onSubmit}>
+                <div className="form-row align-items-center">
+                    <div className="col-auto">
+                        <label className="sr-only" htmlFor="q">Поиск</label>
+                        <input type="text" className="form-control mb-2" name="q" id="q"
+                               placeholder="Введите поисковый запрос..."
+                               onChange={e => setSearchValue(e.target.value)}
+                        />
+                    </div>
+                    <div className="col-auto">
+                        <button type="submit" className="btn btn-primary mb-2">Найти</button>
+                    </div>
+                </div>
+            </form>
+
+            {paginatedObjectList.map((item) => {
+                return (
+                    <div key={item.id}>
+                        <a href={item.get_absolute_url}>{item.title}</a>
+                    </div>
+                )
+            })}
+        </Layout>
+    )
 }
 
 export default SearchPage;
