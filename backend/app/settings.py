a869fda784f9ae1e961310b4d82968c7fa244144
crusat <crusat@yandex.ru>
2021-11-02 15:15:28 +0300

    login vk, fb

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -30,7 +30,17 @@ CHOICE_MENU_TYPES = [(k, v['title']) for k, v in MENU_TYPES.items()]
 
 SOCIAL_AUTH_AUTHENTICATION_BACKENDS = [
     'social_core.backends.vk.VKOAuth2',  # for client backend: vk-oauth2
+    'social_core.backends.facebook.FacebookOAuth2',  # for client backend: facebook
 ]
 
 SOCIAL_AUTH_VK_OAUTH2_KEY = env('SOCIAL_AUTH_VK_OAUTH2_KEY')
 SOCIAL_AUTH_VK_OAUTH2_SECRET = env('SOCIAL_AUTH_VK_OAUTH2_SECRET')
+
+SOCIAL_AUTH_FACEBOOK_KEY = env('SOCIAL_AUTH_FACEBOOK_KEY')
+SOCIAL_AUTH_FACEBOOK_SECRET = env('SOCIAL_AUTH_FACEBOOK_SECRET')
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+  'locale': 'ru_RU',
+  'fields': 'id, name, email'
+}
+SOCIAL_AUTH_FACEBOOK_API_VERSION = '2.10'
